f2875eaead83481a1fba8223ee976ff039730d03
Suchwinder Singh <windersingh80@yahoo.com>
2020-01-12 23:47:39 -0500

    Branch for Single Student
    Added the tasks I must comeplte in this SingleStudentView.js

---
+++ src/components/view/SingleStudentView.js
@@ -0,0 +1,8 @@
+// - [ ] Write a component to display a single student with the following information:
+//   - [ ] The student's full name, email, image, and gpa
+//   - [ ] The name of their campus (or a helpful message if they don't have one)
+// - [ ] Display the appropriate student when the url matches `/students/:studentId`
+// - [ ] Clicking on a student from the all-students view should navigate to show that student in the single-student view
+
+// - [ ] Clicking on the name of a student in the single-campus view should navigate to show that student in the single-student view
+// - [ ] Clicking on the name of a campus in the single-student view should navigate to show that campus in the single-campus view

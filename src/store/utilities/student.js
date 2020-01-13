adcef0884d50ef29c5589a615fcf8d70e5ca41ac
Melissa <lynch.melissat@gmail.com>
2020-01-13 12:04:33 -0500

    started util/studnet

---
+++ src/store/utilities/student.js
@@ -0,0 +1,41 @@
+// ACTION TYPES;
+const DELETE_STUDENT = "DELETE_STUDENT";
+const EDIT_STUDENT = "EDIT_STUDENT";
+
+// ACTION CREATOR;
+const deleteStudent = (id) => {
+    return {
+        type: DELETE_STUDENT,
+        payload: id
+    }
+}
+
+const editStudent = (student) => {
+    return {
+        type: EDIT_STUDENT,
+        payload: student
+    }
+}
+
+// THUNK CREATOR;
+export const deleteStudentThunk = (id) => (dispatch) => {
+    let resolvedActionObject = deleteStudent(id); 
+    dispatch(resolvedActionObject);
+}
+
+export const editStudentThunk = (student) => (dispatch) => {
+    let resolvedActionObject = editStudent(student); 
+    dispatch(resolvedActionObject);
+}
+
+// REDUCER FUNCTION;
+export default (state = [], action) => {
+    switch (action.type) {
+        case DELETE_STUDENT:
+            return state.filter(student => student.id !== action.payload);
+        case EDIT_STUDENT:
+            return [...state, action.payload]
+        default:
+            return state;
+    }
+}
